c5532f1fbaf2022438bcd9b25545544dfab285eb
viganselmani10-lang <vigan.selmani10@gmail.com>
2026-05-08 12:48:47 +0200

    Update task1.cpp

--- task1.cpp
+++ task1.cpp
@@ -3,5 +3,7 @@ using namespace std;
 int main() {
 cout << "Hello GitHub!" << endl;
 cout << "This is my first C++ file." << endl;
+  cout << "I am learning C++ and GitHub." << endl;
+cout << "This is my second commit." << endl;
 return 0;
 }
